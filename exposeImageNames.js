4f4e66f74f696524ba29dfc41df1f092fca031fd
Brett Knights <brett@knightsofthenet.com>
2016-12-27 14:55:51 -0800

    handle matrix child items

--- exposeImageNames.js
+++ exposeImageNames.js
@@ -7,18 +7,53 @@ var kotnShadowImageNames;
      * @return {void}
      */
     function itemBeforeSubmit(type) {
-        if (type == 'edit' || type == 'create' || type == 'xedit') {
+        if (type == 'edit' || type == 'create') {
             var imageName = nlapiGetFieldText('storedisplayimage');
-            var shadowImage = nlapiGetFieldValue('custitem_kotn_image_name');
-            if (imageName && imageName != shadowImage)
+            if (imageName && imageName != nlapiGetFieldValue('custitem_kotn_image_name'))
                 nlapiSetFieldValue('custitem_kotn_image_name', imageName);
             var thumbnailName = nlapiGetFieldText('storedisplaythumbnail');
-            var shadowthumbnail = nlapiGetFieldValue('custitem_kotn_thumbnail_name');
-            if (thumbnailName && thumbnailName != shadowthumbnail)
+            if (thumbnailName && thumbnailName != nlapiGetFieldValue('custitem_kotn_thumbnail_name'))
                 nlapiSetFieldValue('custitem_kotn_thumbnail_name', thumbnailName);
         }
     }
     kotnShadowImageNames.itemBeforeSubmit = itemBeforeSubmit;
+    function itemAfterSubmit(type) {
+        nlapiLogExecution('DEBUG', type + ' after submit with matrix type: ' + nlapiGetFieldValue('matrixtype'));
+        if (type == 'edit' || type == 'create' || type == 'xedit') {
+            if (!nlapiGetContext().getFeature('MATRIXITEMS'))
+                return;
+            var oldRec = nlapiGetOldRecord();
+            var imageName = nlapiGetFieldValue('custitem_kotn_image_name');
+            if (imageName && oldRec && oldRec.getFieldValue('custitem_kotn_image_name') == imageName)
+                imageName = null;
+            var thumbnailName = nlapiGetFieldValue('custitem_kotn_thumbnail_name');
+            if (thumbnailName && oldRec && oldRec.getFieldValue('custitem_kotn_thumbnail_name') == thumbnailName)
+                thumbnailName = null;
+            if (imageName || thumbnailName) {
+                var childItems = nlapiSearchRecord('item', null, [
+                    new nlobjSearchFilter('parent', null, 'is', nlapiGetRecordId()),
+                    new nlobjSearchFilter('matrixchild', null, 'is', 'T'),
+                    new nlobjSearchFilter('isinactive', null, 'is', 'F')
+                ]);
+                if (childItems) {
+                    var updateFields = [], updateValues = [];
+                    if (imageName) {
+                        updateFields.push('custitem_kotn_image_name');
+                        updateValues.push(imageName);
+                    }
+                    if (thumbnailName) {
+                        updateFields.push('custitem_kotn_thumbnail_name');
+                        updateValues.push(thumbnailName);
+                    }
+                    nlapiLogExecution("DEBUG", "Updating " + childItems.length + " for image names");
+                    childItems.forEach(function (c) {
+                        nlapiSubmitField(c.getRecordType(), c.getId(), updateFields, updateValues, { disabletriggers: true, enablesourcing: false });
+                    });
+                }
+            }
+        }
+    }
+    kotnShadowImageNames.itemAfterSubmit = itemAfterSubmit;
     /**
      * prefill these fields after install. You'll need a deployment
      * for each type of item. e.g., Inventory Item, Non-Inventory Item, Serialized Item
@@ -29,19 +64,48 @@ var kotnShadowImageNames;
         var item = nlapiLoadRecord(recType, recId);
         var needSave = false;
         var imageName = item.getFieldText('storedisplayimage') || '';
-        var shadowImage = item.getFieldValue('custitem_kotn_image_name') || '';
-        if (imageName && imageName != shadowImage) {
+        if (imageName && imageName != item.getFieldValue('custitem_kotn_image_name')) {
             item.setFieldValue('custitem_kotn_image_name', imageName);
             needSave = true;
         }
         var thumbnailName = item.getFieldText('storedisplaythumbnail');
-        var shadowthumbnail = item.getFieldValue('custitem_kotn_thumbnail_name');
-        if (thumbnailName && thumbnailName != shadowthumbnail) {
+        if (thumbnailName && thumbnailName != item.getFieldValue('custitem_kotn_thumbnail_name')) {
             item.setFieldValue('custitem_kotn_thumbnail_name', thumbnailName);
             needSave = true;
         }
         if (needSave) {
             nlapiSubmitRecord(item, { disabletriggers: true, ignoremandatoryfields: true, enablesourcing: false });
+        }
+        if (!nlapiGetContext().getFeature('MATRIXITEMS'))
+            return;
+        var filters = [
+            new nlobjSearchFilter('parent', null, 'is', recId),
+            new nlobjSearchFilter('matrixchild', null, 'is', 'T'),
+            new nlobjSearchFilter('isinactive', null, 'is', 'F')
+        ];
+        //	    var imageClause = imageName ? "when '"+ imageName +"' != {custitem_kotn_image_name} then 1 " : '';
+        //	    var thumbClause = thumbnailName ? "when '"+ thumbnailName +"' != {custitem_kotn_thumbnail_name} then 1 " : '';
+        //		if(imageClause || thumbClause){
+        //			filters.push(new nlobjSearchFilter('formulanumeric', null, 'equalto', '1').
+        //				setFormula("case "+ imageClause +" "+ thumbClause +" else 0 end"));
+        //
+        //			nlapiLogExecution('DEBUG', 'using formula to find children to update', filters.slice(-1)[0].getFormula());
+        //		}
+        var childItems = nlapiSearchRecord('item', null, filters);
+        if (childItems) {
+            var updateFields = [], updateValues = [];
+            if (imageName) {
+                updateFields.push('custitem_kotn_image_name');
+                updateValues.push(imageName);
+            }
+            if (thumbnailName) {
+                updateFields.push('custitem_kotn_thumbnail_name');
+                updateValues.push(thumbnailName);
+            }
+            nlapiLogExecution('DEBUG', 'applying image update to ' + childItems.length, JSON.stringify(updateFields) + '\n\twith\n' + JSON.stringify(updateValues));
+            childItems.forEach(function (c) {
+                nlapiSubmitField(c.getRecordType(), c.getId(), updateFields, updateValues, { disabletriggers: true, enablesourcing: false });
+            });
         }
     }
     kotnShadowImageNames.massImages = massImages;
--- exposeImageNames.ts
+++ exposeImageNames.ts
@@ -8,15 +8,49 @@ module kotnShadowImageNames{
 	 * @return {void}
 	 */
 	export function itemBeforeSubmit(type) {
-	    if (type == 'edit' || type == 'create' || type == 'xedit') {
+	    if (type == 'edit' || type == 'create') {
 	        var imageName = nlapiGetFieldText('storedisplayimage');
-	        var shadowImage = nlapiGetFieldValue('custitem_kotn_image_name'); 
-	        if (imageName && imageName != shadowImage) nlapiSetFieldValue('custitem_kotn_image_name', imageName);
+	        if (imageName && imageName != nlapiGetFieldValue('custitem_kotn_image_name')) nlapiSetFieldValue('custitem_kotn_image_name', imageName);
 
 	        var thumbnailName = nlapiGetFieldText('storedisplaythumbnail');
-	        var shadowthumbnail = nlapiGetFieldValue('custitem_kotn_thumbnail_name'); 
-	        if (thumbnailName && thumbnailName != shadowthumbnail) nlapiSetFieldValue('custitem_kotn_thumbnail_name', thumbnailName);
+	        if (thumbnailName && thumbnailName != nlapiGetFieldValue('custitem_kotn_thumbnail_name')) nlapiSetFieldValue('custitem_kotn_thumbnail_name', thumbnailName);
 	    }
+	}
+
+	export function itemAfterSubmit(type){
+		nlapiLogExecution('DEBUG', type +' after submit with matrix type: '+ nlapiGetFieldValue('matrixtype'));
+		if(type == 'edit' || type == 'create' || type == 'xedit'){
+			if(!nlapiGetContext().getFeature('MATRIXITEMS') ) return;
+			var oldRec = nlapiGetOldRecord();
+			var imageName = nlapiGetFieldValue('custitem_kotn_image_name');
+			if(imageName && oldRec && oldRec.getFieldValue('custitem_kotn_image_name') == imageName) imageName = null;
+
+			var thumbnailName = nlapiGetFieldValue('custitem_kotn_thumbnail_name');
+			if(thumbnailName && oldRec && oldRec.getFieldValue('custitem_kotn_thumbnail_name') == thumbnailName) thumbnailName = null;
+
+			if(imageName || thumbnailName){
+				var childItems = nlapiSearchRecord('item', null, [
+					new nlobjSearchFilter('parent', null, 'is', nlapiGetRecordId()),
+					new nlobjSearchFilter('matrixchild', null, 'is', 'T'),
+					new nlobjSearchFilter('isinactive', null, 'is', 'F')
+				]);
+				if(childItems) {
+					var updateFields : string[] = [], updateValues : string[] = [];
+					if(imageName){
+						updateFields.push('custitem_kotn_image_name');
+						updateValues.push(imageName);
+					}
+					if(thumbnailName){
+						updateFields.push('custitem_kotn_thumbnail_name');
+						updateValues.push(thumbnailName);
+					}
+					nlapiLogExecution("DEBUG", "Updating " + childItems.length +" for image names");
+					childItems.forEach(function(c){
+						nlapiSubmitField(c.getRecordType(), c.getId(), updateFields, updateValues, {disabletriggers:true, enablesourcing:false});
+					});
+				}
+			}
+		}
 	}
 
 	/**
@@ -30,15 +64,13 @@ module kotnShadowImageNames{
 	    var needSave = false;
 
 	    var imageName = item.getFieldText('storedisplayimage') || '';
-        var shadowImage = item.getFieldValue('custitem_kotn_image_name') || ''; 
-        if (imageName && imageName != shadowImage){
+        if (imageName && imageName != item.getFieldValue('custitem_kotn_image_name')){
         	item.setFieldValue('custitem_kotn_image_name', imageName);
         	needSave = true;
         } 
 
         var thumbnailName = item.getFieldText('storedisplaythumbnail');
-        var shadowthumbnail = item.getFieldValue('custitem_kotn_thumbnail_name'); 
-        if (thumbnailName && thumbnailName != shadowthumbnail){
+        if (thumbnailName && thumbnailName != item.getFieldValue('custitem_kotn_thumbnail_name')){
         	item.setFieldValue('custitem_kotn_thumbnail_name', thumbnailName);
         	needSave = true;
         }
@@ -46,5 +78,40 @@ module kotnShadowImageNames{
 	    if (needSave) {
 	        nlapiSubmitRecord(item, {disabletriggers:true,ignoremandatoryfields:true, enablesourcing:false});
 	    }
+
+	    if(!nlapiGetContext().getFeature('MATRIXITEMS') ) return;
+
+	    var filters = [
+			new nlobjSearchFilter('parent', null, 'is', recId),
+			new nlobjSearchFilter('matrixchild', null, 'is', 'T'),
+			new nlobjSearchFilter('isinactive', null, 'is', 'F')
+		];
+
+//	    var imageClause = imageName ? "when '"+ imageName +"' != {custitem_kotn_image_name} then 1 " : '';
+//	    var thumbClause = thumbnailName ? "when '"+ thumbnailName +"' != {custitem_kotn_thumbnail_name} then 1 " : '';
+//		if(imageClause || thumbClause){
+//			filters.push(new nlobjSearchFilter('formulanumeric', null, 'equalto', '1').
+//				setFormula("case "+ imageClause +" "+ thumbClause +" else 0 end"));
+//
+//			nlapiLogExecution('DEBUG', 'using formula to find children to update', filters.slice(-1)[0].getFormula());
+//		}
+		
+		var childItems = nlapiSearchRecord('item', null, filters);
+		if(childItems) {
+			var updateFields : string[] = [], updateValues : string[] = [];
+			if(imageName){
+				updateFields.push('custitem_kotn_image_name');
+				updateValues.push(imageName);
+			}
+			if(thumbnailName){
+				updateFields.push('custitem_kotn_thumbnail_name');
+				updateValues.push(thumbnailName);
+			}
+			nlapiLogExecution('DEBUG', 'applying image update to '+ childItems.length, JSON.stringify(updateFields) +'\n\twith\n'+ JSON.stringify(updateValues));
+			childItems.forEach(function(c){
+				nlapiSubmitField(c.getRecordType(), c.getId(),updateFields, updateValues,{disabletriggers:true, enablesourcing:false});
+			});
+		}
+					  
 	}
 }
